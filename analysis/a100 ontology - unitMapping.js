e0d3819dd8870d1e158688c7a12c969dfa1354a2
Sirko Schindler <sirko.schindler@uni-jena.de>
2017-06-19 10:43:06 +0200

    added option to filter sameAs relations to certain ontologies

--- analysis/a100 ontology - unitMapping.js
+++ analysis/a100 ontology - unitMapping.js
@@ -17,42 +17,60 @@ var localCfg = {
     },
     log = function( msg, type ) {
       Log( localCfg.moduleName, msg, type );
-    };
+    },
+    restrictTo = false;
+//    restrictTo = [ 'OM', 'OM2' ]; // restrict output to listed ontologies; set to false for all ontologies
 
 function ontologyUnitMapping() {
-  
+
   // ontology in turtle syntax
   var turtle = '@prefix owl: <http://www.w3.org/2002/07/owl#> .\n';
   turtle += '\n';
-      
+
   // load unitSynSets
   var unitSynSets = OntoStore.getResult( 'mapUnit' );
-  
+
   for( var unitSynSet of unitSynSets ) {
-    
+
     // get list of synonyms
     var units = unitSynSet.getSynonyms();
-    
+
     if (units.length > 1) {
       // iterate synonym pairs
       for (var i = 0; i < units.length ; i++) {
+
+        // maybe skip this unit
+        if( restrictTo && !restrictTo.includes( units[i].getOntology() ) ) {
+          continue;
+        }
+
         for (var j = i+1; j < units.length ; j++) {
-            // add same as triple
-            turtle += '<' + units[i].uri + '> owl:sameAs <' + units[j].uri + '> .\n';
+
+          // maybe skip this unit
+          if( restrictTo && !restrictTo.includes( units[j].getOntology() ) ) {
+            continue;
+          }
+
+          // add same as triple
+          turtle += '<' + units[i].getURI() + '> owl:sameAs <' + units[j].getURI() + '> .\n';
+
         }
       }
       turtle += '\n';
     }
 
   }
-  
+
+  // remove triple line breaks
+  turtle = turtle.replace( /\n\n\n+/g, "\n\n" );
+
   // write file
   var file =  Cfg.targetPath + localCfg.moduleKey + ' ' + localCfg.moduleName + '.ttl';
   Fs.writeFileSync( file, turtle );
   log( 'written ' + file );
 
   // done
-  return Q( true );    
+  return Q( true );
 
 }
 
@@ -60,7 +78,7 @@ function ontologyUnitMapping() {
 
 // if called directly, execute, else export
 if(require.main === module) {
-  ontologyUnitMapping().done(); 
-} else { 
-  module.exports = ontologyUnitMapping; 
+  ontologyUnitMapping().done();
+} else {
+  module.exports = ontologyUnitMapping;
 }
